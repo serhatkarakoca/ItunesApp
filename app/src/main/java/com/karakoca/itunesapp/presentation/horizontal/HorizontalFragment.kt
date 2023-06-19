159491ca737e7d725508aa94a2e2b7e310110c96
Serhat Karakoca <serhatkarakoca@gmail.com>
2023-06-19 11:04:06 +0300

    showing snackbar after deletion

--- app/src/main/java/com/karakoca/itunesapp/presentation/horizontal/HorizontalFragment.kt
+++ app/src/main/java/com/karakoca/itunesapp/presentation/horizontal/HorizontalFragment.kt
@@ -1,9 +1,11 @@
 package com.karakoca.itunesapp.presentation.horizontal
 
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.karakoca.core.view.BaseFragment
 import com.karakoca.itunesapp.R
 import com.karakoca.itunesapp.databinding.FragmentHorizontalBinding
@@ -46,10 +48,21 @@ class HorizontalFragment :
             .setMessage(getString(R.string.are_u_sure_delete))
             .setPositiveButton(getString(android.R.string.ok)) { _, _ ->
                 viewModel.deleteMusicItem(trackId)
+                showDeletedBar()
             }
             .setNegativeButton(getString(android.R.string.cancel)) { dialog, _ -> dialog.dismiss() }
             .show()
 
+    }
+
+    private fun showDeletedBar() {
+        Snackbar.make(
+            requireView(),
+            getString(R.string.deleted_info),
+            Snackbar.ANIMATION_MODE_SLIDE
+        ).setBackgroundTint(ContextCompat.getColor(requireContext(), R.color.white))
+            .setTextColor(ContextCompat.getColor(requireContext(), R.color.purple_500))
+            .setAnchorView(R.id.bottom_nav_view).show()
     }
 
     private fun itemClickListener(item: SearchResult) {
